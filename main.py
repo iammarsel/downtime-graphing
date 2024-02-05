a5b512ca81b18cfaec827d512b8323886b6d4087
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2024-02-04 22:57:39 -0500

    optimized with functions

--- main.py
+++ main.py
@@ -28,26 +28,43 @@ mapping = {
     '2048QAM': 246
 }
 
-outputs = {
-    '1': {},
-    '2': {},
-    '3': {},
-    '4': {},
-    '5': {},
-    '6': {},
-    '7': {},
-    '8': {},
-    '11': {},
-    '12': {}}
+outputs = {str(i): {} for i in range(1, 13)}
 times = {}
 titles = {}
+modems = []
 
 
 def choose_file():
-    global filename
+    global filename 
     filename = filedialog.askopenfilename(
         initialdir="/", title="Select a File", filetypes=(("All files", "*.*"), ("Text files", "*.txt*")))
 
+def plot_graph(ax, y_data, total_data, modem_data):
+    temp1, temp2, temp3 = '', '', ''
+    temp1 = modem_data[0]
+    temp2 = modem_data[1]
+    temp3 = ""
+    if len(modem_data) > 2:
+        temp3 = modem_data[2]
+    if len(y_data) > 0 and len(total_data[1]) > 0:
+        locator = mdates.AutoDateLocator()
+        ax.title.set_text(
+                titles[temp1]+"     "+"Total time under "+threshold.get()+" Mb/s: "+str(total_data[2])+" seconds")
+        ax.set_facecolor('#cccccc')
+        ax.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(locator))
+        ax.plot(total_data[0], y_data[0][0], label="Modem "+temp1+" Speed")
+        ax.plot(total_data[0], y_data[1], label="Modem "+temp2+" Speed")
+        if temp3 != '':
+            ax.plot(total_data[0], y_data[2], label="Modem "+temp3+" Speed")
+        ax.plot(total_data[0], y_data[1], label="Total Speed")
+        ax.set_alpha(0.5)
+        ax.legend(loc="lower right")
+        ax.set_ylim(0, max(y_data[1])+50)
+        ax.grid(True)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Mb/s')
 
 def create_graph(m):
     print(times)
@@ -122,121 +139,55 @@ def create_graph(m):
             down_time += (totalx[i]-totalx[i-1]).total_seconds()
     return [y1, y2, y3], [totalx, totaly, down_time]
 
-
 def start_analysis():
-    data1 = g1.get().split(",")
-    data2 = g2.get().split(",")
-    data3 = g3.get().split(",")
     global outputs, times, titles
-    outputs = {
-    '1': {},
-    '2': {},
-    '3': {},
-    '4': {},
-    '5': {},
-    '6': {},
-    '7': {},
-    '8': {},
-    '11': {},
-    '12': {}}
+    outputs = {str(i): {} for i in range(1, 13)}
     times = {}
-    # try:
-    month_val = month.get()
-    year_val = year.get()
-    with open(filename) as f:
-        for line in f.read().splitlines():
-            line = line.split(",")
-            event_time = line[1]
-            date_time_obj = datetime.strptime(event_time, '%Y/%m/%d %H:%M:%S')
-            modulation = line[9]
-            slot_num = line[5]
-            #print(line, line[10].strip() in mapping, len(slot_num) == 6 and date_time_obj.month == int(month_val), date_time_obj.year == int(year_val) and (checkmarks['TX'].get() == 1 and modulation == "TX Modulation") or (checkmarks['RX'].get() == 1 and modulation == "RX Modulation"))
-            if line[10].strip() in mapping and len(slot_num) == 6 and date_time_obj.month == int(month_val) and date_time_obj.year == int(year_val) and ((checkmarks['TX'].get() == 1 and modulation == "TX Modulation") or (checkmarks['RX'].get() == 1 and modulation == "RX Modulation")):
-                print(slot_num[5], 'confirmed')
-                speed = mapping[line[10].strip()]
-                if "0" in slot_num:
-                    titles[slot_num[5]] = line[8]
-                    if slot_num[5] in times:
-                        times[slot_num[5]].append(date_time_obj)
+    try:
+        month_val = month.get()
+        year_val = year.get()
+        with open(filename) as f:
+            for line in f.read().splitlines():
+                line = line.split(",")
+                event_time = line[1]
+                date_time_obj = datetime.strptime(event_time, '%Y/%m/%d %H:%M:%S')
+                modulation = line[9]
+                slot_num = line[5]
+                if line[10].strip() in mapping and len(slot_num) == 6 and date_time_obj.month == int(month_val) and date_time_obj.year == int(year_val) and ((checkmarks['TX'].get() == 1 and modulation == "TX Modulation") or (checkmarks['RX'].get() == 1 and modulation == "RX Modulation")):
+                    print(slot_num[5], 'confirmed')
+                    speed = mapping[line[10].strip()]
+                    if "0" in slot_num:
+                        titles[slot_num[5]] = line[8]
+                        if slot_num[5] in times:
+                            times[slot_num[5]].append(date_time_obj)
+                        else:
+                            times[slot_num[5]] = [date_time_obj]
+                        outputs[slot_num[5]][date_time_obj] = speed
                     else:
-                        times[slot_num[5]] = [date_time_obj]
-                    outputs[slot_num[5]][date_time_obj] = speed
-                else:
-                    titles[slot_num[4:6]] = line[8]
-                    if slot_num[4:6] in times:
-                        times[slot_num[4:6]].append(date_time_obj)
-                    else:
-                        times[slot_num[4:6]] = [date_time_obj]
-                    outputs[slot_num[4:6]][date_time_obj] = speed
-    print()
-    print(titles)               
-    # except:
-    #  print("Error Occured, try again!")
-    #  return
+                        titles[slot_num[4:6]] = line[8]
+                        if slot_num[4:6] in times:
+                            times[slot_num[4:6]].append(date_time_obj)
+                        else:
+                            times[slot_num[4:6]] = [date_time_obj]
+                        outputs[slot_num[4:6]][date_time_obj] = speed            
+    except:
+        print("Error Occured, try again!")
+        return
     # sorting outputs of all ports
     for k in outputs.keys():
-        outputs[k] = {i: outputs[k][i]
-                      for i in sorted(list(outputs[k].keys()))}
-
-    modems1, totals1 = create_graph(data1)
-    modems2, totals2 = create_graph(data2)
-    modems3, totals3 = [], []
-    if len(data3) > 0:
-        modems3, totals3 = create_graph(data3)
-
-    graph_info = [[modems1, totals1], [modems2, totals2], [modems3, totals3]]
+        outputs[k] = {i: outputs[k][i] for i in sorted(list(outputs[k].keys()))}
     fig, axs = plt.subplots(3, figsize=(12, 12))
-    for i in range(3):
-        temp1, temp2, temp3 = '', '', ''
-        if i == 0:
-            temp1 = data1[0]
-            temp2 = data1[1]
-            temp3 = ""
-            if len(data1) > 2:
-                temp3 = data1[2]
-        elif i == 1:
-            temp1 = data2[0]
-            temp2 = data2[1]
-            temp3 = ""
-            if len(data2) > 2:
-                temp3 = data2[2]
-        else:
-            temp1 = data3[0]
-            temp2 = data3[1]
-            temp3 = ""
-            if len(data3) > 2:
-                temp3 = data3[2]
-        if len(graph_info[i][0]) > 0 and len(graph_info[i][1][1]) > 0:
-            locator = mdates.AutoDateLocator()
-
-            axs[i].title.set_text(
-                titles[temp1]+"     "+"Total time under "+threshold.get()+" Mb/s: "+str(graph_info[i][1][2])+" seconds")
-            axs[i].set_facecolor('#cccccc')
-            axs[i].xaxis.set_minor_locator(mdates.DayLocator(interval=1))
-            axs[i].xaxis.set_major_locator(locator)
-            axs[i].xaxis.set_major_formatter(mdates.AutoDateFormatter(locator))
-            axs[i].plot(graph_info[i][1][0], graph_info[i][0]
-                        [0], label="Modem "+temp1+" Speed")
-            axs[i].plot(graph_info[i][1][0], graph_info[i][0]
-                        [1], label="Modem "+temp2+" Speed")
-            if temp3 != '':
-                axs[i].plot(graph_info[i][1][0], graph_info[i]
-                                [0][2], label="Modem "+temp3+" Speed")
-            axs[i].plot(graph_info[i][1][0], graph_info[i]
-                        [1][1], label="Total Speed")
-            axs[i].set_alpha(0.5)
-            axs[i].legend(loc="lower right")
-            axs[i].set_ylim(0, max(graph_info[i][1][1])+50)
-            axs[i].grid(True)
-            axs[i].set_xlabel('Time')
-            axs[i].set_ylabel('Mb/s')
-
+    for i,m in enumerate(modems):
+        modem_data = m.get().split(",")
+        if len(m) > 0:
+            y_data,total_data = create_graph(modem_data)
+            plot_graph(axs[i], y_data, total_data, modem_data)
+            
     for l in fig.gca().lines:
         l.set_alpha(.7)
     plt.subplots_adjust(left=0.05, bottom=0.06, right=0.95,
                         top=0.95, wspace=0.2, hspace=0.2)
     plt.show()
-
 
 greeting = CTkLabel(window, text="Speed Alarm Graphs and Summary for Modems")
 greeting.place(relx=0.5, rely=0.1, anchor=CENTER)
@@ -244,11 +195,9 @@ greeting.place(relx=0.5, rely=0.1, anchor=CENTER)
 file_button = CTkButton(window, text="Choose your Event Log file", command=choose_file,
                         fg_color="#119149", hover_color="#45ba78")
 file_button.place(relx=0.5, rely=0.2, anchor=CENTER)
-month = CTkEntry(master=window, placeholder_text="Enter a month",
-                 width=120, height=25, border_width=2, corner_radius=10)
+month = CTkEntry(master=window, placeholder_text="Enter a month", width=120, height=25, border_width=2, corner_radius=10)
 month.place(relx=0.5, rely=0.3, anchor=CENTER)
-year = CTkEntry(master=window, placeholder_text="Enter a year",
-                width=120, height=25, border_width=2, corner_radius=10)
+year = CTkEntry(master=window, placeholder_text="Enter a year", width=120, height=25, border_width=2, corner_radius=10)
 year.place(relx=0.5, rely=0.4, anchor=CENTER)
 
 tx = CTkCheckBox(master=window, text="TX", variable=checkmarks['TX'])
@@ -257,25 +206,12 @@ tx.place(relx=0.4, rely=0.5, anchor=CENTER)
 rx = CTkCheckBox(master=window, text="RX", variable=checkmarks['RX'])
 rx.place(relx=0.6, rely=0.5, anchor=CENTER)
 
-g1label = CTkLabel(window, text="Graph 1 Modems: ")
-g1label.place(relx=0.3, rely=0.6, anchor=CENTER)
-
-g2label = CTkLabel(window, text="Graph 2 Modems: ")
-g2label.place(relx=0.3, rely=0.7, anchor=CENTER)
-
-g3label = CTkLabel(window, text="Graph 3 Modems: ")
-g3label.place(relx=0.3, rely=0.8, anchor=CENTER)
-
-
-
-g1 = CTkEntry(master=window, placeholder_text="Enter modems (seperated by commas)", width=120, height=25, border_width=2, corner_radius=10)
-g1.place(relx=0.5, rely=0.6, anchor=CENTER)
-
-g2 = CTkEntry(master=window, placeholder_text="Enter modems (seperated by commas)", width=120, height=25, border_width=2, corner_radius=10)
-g2.place(relx=0.5, rely=0.7, anchor=CENTER)
-
-g3 = CTkEntry(master=window, placeholder_text="Enter modems (seperated by commas)", width=120, height=25, border_width=2, corner_radius=10)
-g3.place(relx=0.5, rely=0.8, anchor=CENTER)
+for i in range(3):
+    glabel = CTkLabel(window, text="Graph "+str(i+1)+" Modems: ")
+    glabel.place(relx=0.3, rely=0.6+(i*0.1), anchor=CENTER)
+    m_entry = CTkEntry(master=window, placeholder_text="Enter modems (seperated by commas)", width=120, height=25, border_width=2, corner_radius=10)
+    m_entry.place(relx=0.5, rely=0.6+(i*0.1), anchor=CENTER)
+    modems.append(m_entry)
 
 threshold = CTkEntry(master=window, placeholder_text="Enter threshold", width=120, height=25, border_width=2, corner_radius=10)
 threshold.place(relx=0.5, rely=0.9, anchor=CENTER)
